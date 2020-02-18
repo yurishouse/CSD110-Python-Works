c8e4366740fd82f4df79fa538344d02c5a693b63
Shiina Yuri <yurishouse@gmail.com>
2020-02-18 14:44:13 -0800

    2/18/2020 (3)

--- in-class/Ch-7-lists-2.py
+++ in-class/Ch-7-lists-2.py
@@ -24,3 +24,44 @@ if item in numbers:
 else:
     print("Kahoot!")
 
+
+# populate an empty list
+# define an empty list
+# use a loop to request values from the user
+# and add it to the list
+lists = []
+"""
+for i in range(5):
+    lists.append(eval(input("please input a number: ")))
+print(lists)
+"""
+"""
+keep_going = True
+
+items = []
+while keep_going:
+    lists.append(eval(input("please input a number: ")))
+    keep_going = eval(input("Do you want to continue True/False: "))
+
+print(lists)
+"""
+# Entering the values of a list in one go: Building a list from user without a loop
+data = eval(input("Enter your values"))
+
+print(data)
+
+data.insert(3, 1000)  # insert at index 3
+
+print(data.index(1000))
+
+data.sort()
+
+print(data)
+
+data.reverse
+
+print(data)
+
+del data[3]
+
+print(data)
