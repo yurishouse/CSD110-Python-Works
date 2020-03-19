31c53530b81fed6e9d057e38e86c37f63355132b
Shiina Yuri <yurishouse@gmail.com>
2020-03-19 03:49:34 -0700

    kill me plz

---
+++ homeworks/Asm-7-Find-frequent-character.py
@@ -0,0 +1,24 @@
+# Made by Zhongli Liang
+
+
+sentence = input('Enter some words here: ')  # Let user input a string
+
+sentence = sentence.lower()  # convert everything to lower case
+
+count = 0  # Setup a count variable to be use in the loop
+total_count = 0  # Setup a total counter
+
+most_freq_character = ""   # Setup a variable that will store the final result
+
+for ch in sentence:  # Used to zero out the count variable during each loop cycle,
+    # also decide which character is looping
+    for string in sentence:  # A second loop
+        if string == ch:  # if the character that's currently looping is found, it adds 1 to the count variable
+            count += 1
+    if count > total_count:  # compare the most frequent characters in terms oif times
+        total_count = count
+        most_freq_character = ch  # Set the Most frequent character
+    count = 0  # Zero out counter during each cycle
+
+print("The most frequent character is", most_freq_character, "and it appears", total_count, "times.")
+# Print the result
---
+++ homeworks/Asm-7-Pig-Latin.py
@@ -0,0 +1,11 @@
+# Made by Zhongli Liang
+
+string = input("Input Sentence in ALL CAPS:")  # Let user input the sentence, save it as a string
+
+string = string.split()  # Split the string into a list
+
+print("The Sentence in Pig Latin:")  # Tell the user that the line below will be pig latin
+for word in string:   # Loop through the list and change every word to Pig Latin
+    print(word[1:] + word[0] + "AY", end=" ")  # Print out the words as well
+
+print()
